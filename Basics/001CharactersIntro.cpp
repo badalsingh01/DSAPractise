0551a012ec2e54acbf2458e2a66bdaf81cabc6a1
badalsingh01 <deepaktanwar926@gmail.com>
2025-01-07 20:18:23 +0530

    First commit of questions

---
+++ Basics/001CharactersIntro.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	char ch1; // declr.
+	cin >> ch1;
+
+	cout << "ch1 = " << ch1 << endl;
+
+	char ch2;
+	ch2 = 'B'; // assign.
+	cout << "ch2 = " << ch2 << endl;
+
+	char ch3 = 'C'; // initialisation
+	cout << "ch3 = " << ch3 << endl;
+
+	char ch4 = 'a';
+	cout << (int)ch4 << endl;
+
+	char ch5 = 'C';
+	cout << (int)ch5 << endl;
+
+	int x = 'd';
+	cout << x << endl;
+
+	char ch6 = '9';
+	cout << (int)ch6 << endl;
+
+	char ch7 = '8';
+	cout << (int)ch7 << endl;
+	cout << ch7 - '0' << endl;
+
+	return 0;
+}
+// Ascii Values of 0 = 48 and so on 
